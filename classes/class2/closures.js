566976dc855abc707b1137f2f0a57bee5ce2e76f
Evan Dorsky <evan.dorsky@icloud.com>
2015-01-20 23:01:21 -0500

    Remove useless files...

--- classes/class2/closures.js
+++
@@ -1,8 +0,0 @@
-var multSum = function(x, y) {
-	var mult = function() {
-		return x*y
-	}
-	return mult() + x + y
-}
-
-console.log(multSum(5, 6)) // 5*6 + 5 + 6
--- classes/class2/functions.js
+++
@@ -1,8 +0,0 @@
-function func1() {
-	var func2 = function() {
-		return 2;
-	};
-	return func2;
-}
-
-console.log(func1()());
