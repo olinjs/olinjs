fe268ca550c7be72317e2a7cfc925bebd4f537ed
Haley <haley.pelletier@students.olin.edu>
2015-01-23 16:14:02 -0500

    fix

--- classes/class1/learnyounode/exercise11.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var http = require('http');
-
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'content-type':'text/plain'});
-  fs.createReadStream(process.argv[3]).pipe(res);
-})
-
-server.listen(Number(process.argv[2]));
--- classes/class1/learnyounode/exercise7.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var url = process.argv[2];
-
-http.get(url, function(response) {
-  response.setEncoding("utf8");
-  response.on("data",function(data) {
-    console.log(data);
-  })
-});
