930e426aa4b492bde30263208552b2e33b9b8bf2
Rahil <rahil.dedhia@students.olin.edu>
2015-01-23 15:28:20 -0500

    looks like I was on the right track with jsonapi, just forgot to stringify the output. now everything works

--- classes/class0/learnyounode/jsonapi.js
+++ classes/class0/learnyounode/jsonapi.js
@@ -9,17 +9,53 @@ function makeJson (hr, min, sec) {
 		"minute": min,
 		"second": sec
 	}
-	return json_date;
+	return JSON.stringify(json_date);
 }
 
 var server = http.createServer(function (request, response) {
-	response.writeHead(200, { 'Content-Type': 'application/json' });
+	// response.writeHead(200, { 'Content-Type': 'application/json' });
 	info = url.parse(request.url, true);
 	if (info.pathname == path) {
 		var date = new Date(info.query['iso']);
-		console.log(makeJson(date.getHours(), date.getMinutes(), date.getSeconds()));
-		response.end("");
+		response.end(makeJson(date.getHours(), date.getMinutes(), date.getSeconds()));
 	}
 });
 
 server.listen(port);
+
+// Official Solution
+
+// var http = require('http')
+// var url = require('url')
+
+// function parsetime (time) {
+//   return {
+//     hour: time.getHours(),
+//     minute: time.getMinutes(),
+//     second: time.getSeconds()
+//   }
+// }
+
+// function unixtime (time) {
+//   return { unixtime : time.getTime() }
+// }
+
+// var server = http.createServer(function (req, res) {
+//   var parsedUrl = url.parse(req.url, true)
+//   var time = new Date(parsedUrl.query.iso)
+//   var result
+
+//   if (/^\/api\/parsetime/.test(req.url))
+//     result = parsetime(time)
+//   else if (/^\/api\/unixtime/.test(req.url))
+//     result = unixtime(time)
+
+//   if (result) {
+//     res.writeHead(200, { 'Content-Type': 'application/json' })
+//     res.end(JSON.stringify(result))
+//   } else {
+//     res.writeHead(404)
+//     res.end()
+//   }
+// })
+// server.listen(Number(process.argv[2]))
