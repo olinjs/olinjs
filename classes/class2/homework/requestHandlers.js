b3a184ff3b57a59636ebc8f6326a544e51727ec3
Chelsea <chelsea.bailey@students.olin.edu>
2015-01-27 10:45:39 -0800

    full tut

--- classes/class2/homework/requestHandlers.js
+++ classes/class2/homework/requestHandlers.js
@@ -1,20 +1,28 @@
-var exec = require("child_process").exec;
+var querystring = require('querystring');
 
-function start() {
+function start(response, postData) {
 	console.log("Request handler 'start' was called.");
-	var content = "empty";
-	exec("ls -lah", function (error, stdout, stderr) {
-		response.writeHead(200, {'Content-Type': 'text/plain'});
-		response.write(stdout);
-		response.end();
-	});
-	return content;
+	var body = '<html>'+
+	'<head>'+
+	'<meta http-equiv="Content-Type" content="text/html; '+
+	'charset=UTF-8" />'+
+	'</head>'+
+	'<body>'+
+	'<form action="/upload" method="post">'+
+	'<textarea name="text" rows="20" cols="60"></textarea>'+
+	'<input type="submit" value="Submit text" />'+
+	'</form>'+
+	'</body>'+
+	'</html>';
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(body);
+	response.end();
 }
 
-function upload() {
+function upload(response, postData) {
 	console.log("Request handler 'upload' was called.");
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('Hello Upload');
+	response.write('You sent' + querystring.parse(postData).text);
 	response.end();
 }
 
--- classes/class2/homework/server.js
+++ classes/class2/homework/server.js
@@ -3,10 +3,21 @@ var url = require('url');
 
 function start(route,handle) {
 	function onRequest(request, response) {
+		var postData = '';
 		var pathname = url.parse(request.url).pathname;
 		console.log('request for ' + pathname + ' received.');
 
-		route(handle,pathname,response);		
+		request.setEncoding('utf8');
+
+		request.addListener('data', function(postDataChunk) {
+			postData += postDataChunk;
+			console.log('Received POST data chunk ' + postDataChunk + '.');
+		});
+
+		request.addListener('end', function() {
+			route(handle,pathname,response);
+		});
+				
 	}
 
 	http.createServer(onRequest).listen(8888);
