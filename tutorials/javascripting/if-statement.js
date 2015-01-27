d8397c8019616ea31629713e33308bed90a91097
Ryan Eggert <Ryan.Eggert@students.olin.edu>
2015-01-27 14:00:24 -0500

    Assorted tutorials

---
+++ tutorials/javascripting/if-statement.js
@@ -0,0 +1,7 @@
+var fruit = 'orange'
+
+if (length(fruit) > 5) {
+    console.log('The fruit name has more than five characters.')
+} else {
+    console.log('The fruit name has five characters or less.')
+}
