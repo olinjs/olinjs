fbb37b70ba483e1e0b02649583156846b01dd03f
Chelsea <chelsea.bailey@students.olin.edu>
2015-01-30 12:11:11 -0800

    cat app

---
+++ classes/class3/homework/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var index = require('./routes/index');
+
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index.home);
+app.get('/cats', index.cats);
+app.get('/cats/new', index.newcat);
+app.get('/cats/bycolor/:color', index.bycolor);
+app.get('/cats/delete/old', index.old);
+
+app.listen(3000)
