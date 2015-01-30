2a2d27231dcfb97bbadd9cde66ed2ab0e0bd7fa1
Dennis Chen <dennis.chen@students.olin.edu>
2015-01-30 13:15:50 -0500

    finally works. turns out there were brackets in the set config line. kill me.

--- classes/class4/homework/server.js
+++ classes/class4/homework/server.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+
+var app = express();
+
 /*MongoLab Environment vars*/
 var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
 mongoose.connect(mongoURI);
 var PORT = process.env.PORT || 3000;
 
-var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(logger('dev'));
@@ -21,6 +23,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 /*Routes with cats.js*/
 app.use('/cats', require('./routes/cats.js'));
+
+app.get('/test', function(req,res,next){
+    res.send('hello test!');
+});
 
 app.listen(PORT, function() {
   console.log("Application running on port:", PORT);
