d1bd8c005a321f9e09af60d96ef7e64f4b9b1455
jagreene <j.austin.greene@gmail.com>
2015-02-01 22:11:50 -0500

    fixed ingredient post

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -12,19 +12,19 @@ var onError = function(data, status) {
 
 $ingredientform.submit(function(event) {
   console.log("form submitted");
- // event.preventDefault();
-  formData = $ingredientform.serialize();
+  event.preventDefault();
+  //formData = $ingredientform.serialize();
   var oldName = $ingredientform.attr('id');
   var newName = $ingredientform.find("[name='name']").val();
   var price   = $ingredientform.find("[name='price']").val();
   var available = $ingredientform.fin("[name='available']:checked").val() 
-  formData = {
+  var formData = {
     oldName: oldName,
     newName: newName,
     price: price,
     available: available
-   }
-  $.get("postIngredient",formData)
+   };
+  $.get("postIngredient", formData)
     .done(onSuccess)
     .error(onError);
 });
