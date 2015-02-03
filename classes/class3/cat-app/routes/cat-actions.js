3d7c298b08dfc5dd52506217392b813b77e58fb9
Allison Patterson <allison.patterson@students.olin.edu>
2015-02-03 16:43:24 -0500

    updating routes

--- classes/class3/cat-app/routes/cat-actions.js
+++ classes/class3/cat-app/routes/cat-actions.js
@@ -1,23 +1,31 @@
 var path = require("path");
 var catData = require("./catData");
-var Cat = require(path.join(__dirname,"../models/catModel"));
+var Cat = require(path.join(__dirname,"../models/catSchema"));
 
 var cats = {};
 
-//new cat
+//add new cat
 cats.new = function (req,res) {
   var name = //select random entry from /catData.names
-  var age = //select random number from 1-20
+  var age = //select random number from /catData.ages
   var colors = //select random list from /catData.colors
-  cats.insert({'name': name, 'age': age, 'colors': colors});
+  var acat = new Cat({name: name, age: age, colors: colors});
+  acat.save(function (err) {
+    if (err) {
+      console.log("Problem saving " + name + ".");
+    };
+  });
 };
 
 //sort cats
 cats.sort = function (req,res) {
-  var catsSorted = [];
   var param = //requested parameter
   var value = //requested value
-  cats.find(param : value);
+  cats.find({param : value})
+    .sort({param : -1})
+    .exec(function (err, cats) {
+      console.log(cats);
+    });
 };
 
 //delete cat
@@ -26,3 +34,5 @@ cats.delete = function (req,res) {
   var value = //requested value
   cats.remove(param : value);
 };
+
+module.exports = cats;
