ca5a890c8609d1b7c2bb8ffe0aada5d61ffbd6e8
Evan Simpson <esimpson.rescue@gmail.com>
2015-02-10 00:12:36 -0500

    Switch oauth example to twitter

--- classes/class7/word_cloud/app.js
+++ classes/class7/word_cloud/app.js
@@ -1,8 +1,10 @@
 var express = require('express')
   , http = require('http')
+  , https = require('https')
   , path = require('path')
-  , handlebars = require('express-handlebars')
-  , OAuth = require('oauth');
+  , request = require('request')
+  , text = require('./util/text.js')
+  , handlebars = require('express-handlebars');
   
 var app = express();
 
@@ -16,18 +18,56 @@ app.get('/', function(req, res){
   res.render('index');
 });
 
-// Complete this route so that it will
-// use the OAuth package to request an
-// access token from Facebook.
-// Docs: https://www.npmjs.com/package/oauth
-app.get('/authorize', function(req, res){
+app.get('/tweets', function(req, res){
+  var tweetText;
   
-});
+  var query = req.query.q;
+    
+  var twitterAPIHost = 'https://api.twitter.com' 
+  var twitterConsumerKey = process.env.TWITTER_KEY;
+  var twitterConsumerSecret = process.env.TWITTER_SECRET;
+  var credentials = new Buffer(twitterConsumerKey+":"+twitterConsumerSecret).toString('base64');
+  var postData = "grant_type=client_credentials";
+  var postOptions = {
+    url: twitterAPIHost+'/oauth2/token',
+    headers: {
+      'Host': 'api.twitter.com',
+      'Authorization': 'Basic ' + credentials,
+      'content-type' : 'application/x-www-form-urlencoded',
+      'Content-Length': postData.length
+    },
+    body: postData
+  };
 
-app.get('/callback', function(req, res){
-  
+  request.post(postOptions, function(err, resp, body){
+    var bearer_token = JSON.parse(body).access_token;
+    var getOptions = {
+      url: 'https://api.twitter.com/1.1/search/tweets.json?q='+query,
+      headers: {
+        'Host': 'api.twitter.com',
+        'Authorization': 'Bearer '+bearer_token
+      }
+    };
+
+    request.get(getOptions, function(err, resp, body){
+      var words = [];
+      JSON.parse(body).statuses.forEach(function(val, i, arr){
+        words.pushArray(val.text.split(' '));
+      });
+      var word_freqs = JSON.stringify(text.generateWordFreqs(words));
+      res.json(word_freqs);
+    });
+  });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port", app.get('port'))
 });
+
+
+Array.prototype.pushArray = function() {
+  var toPush = this.concat.apply([], arguments);
+  for (var i = 0, len = toPush.length; i < len; ++i) {
+    this.push(toPush[i]);
+  }
+};
