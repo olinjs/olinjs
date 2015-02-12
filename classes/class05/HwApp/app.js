c8cec5349f305b37c22977218d2f580639272a51
jagreene <j.austin.greene@gmail.com>
2015-02-12 06:02:00 -0500

    Starting new homework

---
+++ classes/class05/HwApp/app.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var exphbs  = require("express-handlebars");
+
+var index = require("./routes/index");
+var kitchen  = require("./routes/kitchen");
+
+var app = express();
+
+var PORT = process.env.PORT || 3000
+
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", kitchen.newOrder);
+app.get("/ingredients", kitchen.getIngredients);
+app.get("/order", kitchen.newOrder);
+app.get("/kitchen",kitchen.getOrders);
+
+app.post("/postIngredient", kitchen.postIngredient);
+app.post("/newIngredient", kitchen.newIngredient);
+app.post("/postOrder", kitchen.postOrder);
+app.post("/deleteOrder", kitchen.deleteOrder);
+app.listen(PORT, function() {
+  console.log("App running on port:", PORT);
+});
