c053c87404b44c9189a68bda5ec331229ad202d7
Josh Langowitz <jlangowitz@gmail.com>
2015-02-12 23:09:39 -0500

    mocha intro finished, examples comment out for pulling

--- classes/class08/in_class/tests/server/test.js
+++ classes/class08/in_class/tests/server/test.js
@@ -1,25 +1,25 @@
 
-// Setup our assertion library
-var expect = require('chai').expect;
+// // Setup our assertion library
+// var expect = require('chai').expect;
 
-var index = require('../../routes/index');
+// var index = require('../../routes/index');
 
 
-// Sample tests
-describe("A test suite", function() {
-	// Syncronous
-	it('should pass', function() { 
-		expect(true).to.be.true; 
-	});
+// // Sample tests
+// describe("A test suite", function() {
+// 	// Syncronous
+// 	it('should pass', function() { 
+// 		expect(true).to.be.true; 
+// 	});
 
-	// Async
-	it('should work asyncronously', function(done) {
-		setTimeout(function() {
-			expect(true).to.be.true;
-			done();
-		}, 1000);
-	});
-});
+// 	// Async
+// 	it('should work asyncronously', function(done) {
+// 		setTimeout(function() {
+// 			expect(true).to.be.true;
+// 			done();
+// 		}, 1000);
+// 	});
+// });
 
 // describe("index", function() {
 // 	it('should have an attribute ten equal to 10', function() {
