b3b357cb500ba86a04c5a6ccadaa7a5a51722c38
David Zhu <huanzhendavidzhu@gmail.com>
2015-02-15 12:21:38 -0500

    Correctly set up Mongoose.

--- classes/class06/twoter/routes/index.js
+++ classes/class06/twoter/routes/index.js
@@ -1,12 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+// Models
+var Author = require('../schemas/author');
+var Twote = require('../schemas/twote');
+
 router.get('/', function(req, res, next) {
   res.render('main');
 });
 
 router.get('/login', function(req, res, next) {
   res.render('login');
+});
+
+router.post('/login', function(req, res) {
+
 });
 
 module.exports = router;
