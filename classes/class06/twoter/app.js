262a81d967311b3daad8b2a382b4fba82e3f1d2f
David Zhu <huanzhendavidzhu@gmail.com>
2015-02-16 11:19:16 -0500

    The form can now send tweets.

--- classes/class06/twoter/app.js
+++ classes/class06/twoter/app.js
@@ -5,9 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
+var session = require('express-session');
+var passport = require('./auth');
+
+var mongoose = require('mongoose');
+var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
+mongoose.connect(mongoURI);
 
 var routes = require('./routes/index');
-
 var app = express();
 
 // Template engine.
@@ -24,13 +29,21 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'cTofTbEF2CNWdUT',
+  resave: false,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
-var mongoose   = require('mongoose');
-var mongoURI = process.env.MONGOURI || "mongodb://localhost/test";
-mongoose.connect(mongoURI);
-
 app.use('/', routes);
+
+app.post('/login', passport.authenticate('local', {
+	failureRedirect: '/login',
+	successRedirect: '/'
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- classes/class06/twoter/routes/index.js
+++ classes/class06/twoter/routes/index.js
@@ -2,19 +2,57 @@ var express = require('express');
 var router = express.Router();
 
 // Models
-var Author = require('../schemas/author');
-var Twote = require('../schemas/twote');
+var Author = require('../models/author');
+var Twote = require('../models/twote');
 
 router.get('/', function(req, res, next) {
-  res.render('main');
+	var message;
+	if (req.session.counter) {
+	    req.session.counter++;
+	    message = "Hello again! Thanks for visiting " + req.session.counter + " times";
+	} else {
+	    message = "Hello, thanks for visiting this site!";
+	    req.session.counter = 1;
+	}
+
+	console.log(message);
+
+	// console.log(req.headers.cookie);
+	console.dir(req.session);
+  res.render('main', {
+  	isAuthenticated: req.isAuthenticated(),
+  	user: req.user
+  });
 });
 
-router.get('/login', function(req, res, next) {
+router.post('/', function (req, res) {
+	console.log(req.body.twote);
+});
+
+router.get('/login', function (req, res, next) {
   res.render('login');
 });
 
-router.post('/login', function(req, res) {
+router.post('/login/register', function(req, res) {
+	var authorName = req.body.name;
+	var authorPass = req.body.password;
 
+	// Look if the user already exists.
+	Author.find({name: authorName})
+		.exec(function (err, authors){
+			if (err) throw err;
+			
+			// If not, let's make a new account.
+			if (authors.length === 0) {
+				var newAuthor = new Author ({
+					name: authorName,
+					password: authorPass
+				});
+				newAuthor.save(function (err){
+					if (err) throw err;
+				});
+			}
+		});
 });
 
 module.exports = router;
