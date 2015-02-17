90945bb5f0a1e7b1058633f524362d7f4ac6bc47
Philip Seger <seger.philip@gmail.com>
2015-02-16 22:58:13 -0500

    trying to fix homework submodule

--- classes/class06/homework/models/twoteModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var twoteSchema = mongoose.Schema({
-  user: String,
-  content: String,
-  time: Date
-});
-
-module.exports = mongoose.model('Twote', twoteSchema);
--- classes/class06/homework/models/userModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var userSchema = mongoose.Schema({
-  name: String,
-  twotes: Array,
-  time: Number
-});
-
-module.exports = mongoose.model('User', userSchema);
