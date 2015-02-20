6aae7f95aad7cdfa5e1497eb9f4e8476951880e4
Thomas Nattestad <thomas.nattestad@gmail.com>
2015-02-19 19:43:29 -0500

    Readmes for class10 and lab1 first draft open to comments

--- classes/class10/in_class/public/javascript/main.js
+++ classes/class10/in_class/public/javascript/main.js
@@ -2,10 +2,7 @@ $( document ).ready(function(){
 	$.get('/catsJson', function(json){
 		var source   = $("#cats-template").html();
 		var template = Handlebars.compile(source);
-		console.log(template);
-		console.log(json);
 		var htmlOutput    = template(json);
-		console.log(htmlOutput);
 		$('#content').html(htmlOutput);
 	});
 });
