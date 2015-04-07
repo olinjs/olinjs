7c41863471c6b523c62dbc2a32d8777abe8c689e
Evan Simpson <esimpson.rescue@gmail.com>
2015-04-06 23:43:48 -0400

    Add middleware example file

---
+++ deepdives/02-middleware/example/middleware.js
@@ -0,0 +1,46 @@
+/*
+  Example Authentication Middleware.
+  This is meant to serve as a simple example of how
+  some middleware can be structured.
+  By no means should this middleware ever be used
+  in any self-respecting application.
+*/
+
+function LocalStrategy(options, verify){
+  this._loggedInRedirect = options.loggedInRedirect;
+  this._loggedOutRedirect = options.loggedOutRedirect;
+
+  this._userField = options.userField || 'username';
+  this._passwordField = options.passwordField || 'password';
+
+  this._verify = verify;
+
+  return this;
+};
+
+LocalStrategy.login(req, res, next){
+  if (this._verify(
+    req.body[this._userField], req.body[this._passwordField])
+  ){
+    req.session.user = req.body[this._userField];
+    res.redirect(this._loggedInRedirect);
+  } else {
+    res.redirect(this._loggedOutRedirect);
+  }
+};
+
+LocalStrategy.logout(req, res, next){
+  req.session.user = null;
+
+  res.redirect(this._loggedOutRedirect);
+};
+
+LocalStrategy.isAuth(req, res, next){
+  if (req.session.user){
+    return next();
+  } else {
+    res.redirect(this._loggedOutRedirect);
+  }
+};
+
+module.exports = LocalStrategy;
