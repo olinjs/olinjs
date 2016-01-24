852c21d674b2ec8d619171d22fcc3da955383775
Mac-I Crowell <macicrowell@gmail.com>
2016-01-24 17:21:30 -0500

    completed lesson 2 reading/practice

---
+++ lessons/02-javascript/NodeExploration/index.js
@@ -0,0 +1,9 @@
+var server = require("./server");
+var router = require("./router");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {}
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+server.start(router.route, handle);
