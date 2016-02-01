db9e440606add51d558716112ec446da26b008fe
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2016-01-31 23:59:07 -0500

    Removing duplicate

--- assignments/04-mongo-cats/models/catModel.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var catSchema = mongoose.Schema({
-  name: String,
-  age: Number, 
-  color: String
-});
-
-module.exports = mongoose.model("cat", catSchema); 
