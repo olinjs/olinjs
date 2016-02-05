6f4e20493c57627bd1a8ddcbce49212de8185dd3
Samantha Kumarasena <samantha.kumarasena@students.olin.edu>
2016-02-05 11:22:48 -0500

    Implemented editIngredient, fixed bug where added ingredients could not be edited without refreshing/ingredients could not be edited multiple times in a row

--- assignments/burgers/public/javascripts/main.js
+++ assignments/burgers/public/javascripts/main.js
@@ -2,22 +2,33 @@
 var $addform = $("#Add");
 console.log($addform);
 
+
 var onAddSuccess = function(data, status) {
   console.log('Test')
   console.log(data[0]);
   console.log(data[1]);
+  console.log(data[2]);
   // var text = "<h1>" + data + "</h1>";
   // $("#result").html(text);
 
     var text = 
+    "<div id=\"" + data[2] + "-editresult\">" + 
     "<li>Ingredient " + data[0] + " costs "+ data[1] + "!</li>" + 
-    "<form id=\"{{this.name}}-ajax-form\" action=\"\" method=\"\">" + 
-    "Edit: <input type=\"text\" name=\"name\"/>" +
-            "<input type=\"number\" name=\"price\"/> <br/>"+
-          "<input type=\"submit\" value=\"Submit\">" +
-          "</form>"
+    // "<form id=\"{{this._id}}-ajax-form\" action=\"editIngre\" method=\"\">" + 
+    // "Edit: <input type=\"text\" name=\"name\"/>" +
+    //         "<input type=\"number\" name=\"price\"/> <br/>"+
+    //       "<input type=\"submit\" value=\"Submit\">" +
+    //       "</form>"
 
-    $("#newresult").html(text);
+  "<form id=\"" + data[2] + "-ajax-form\" class=\"editForm\" action=\"editIngredient\" method=\"GET\">" + 
+  "Edit: <input type=\"text\" name=\"name\"/>" +
+          "<input type=\"number\" name=\"price\"/> <br/>"+
+        "<input type=\"submit\" value=\"Submit\">" +
+        "</form>"+
+      "</div>"
+
+    //$("#newresult").html(text);
+    $("#Alldaforms").append(text);
 };
 
 var onError = function(data, status) {
@@ -41,4 +52,58 @@ $addform.submit(function(event) {
   }) 
     .done(onAddSuccess)
     .error(onError);
+});
+
+
+
+var onEditSuccess = function(data, status) {
+  console.log('Test')
+
+  console.log(data[0]);
+  console.log(data[1]);
+  console.log(data[2]);
+  // var text = "<h1>" + data + "</h1>";
+  // $("#result").html(text);
+
+    var text = 
+  "<li>Ingredient " + data[0] + " costs "+ data[1] + "!</li>" + 
+    "<form id=\"" + data[2] + "-ajax-form\" class=\"editForm\" action=\"editIngredient\" method=\"GET\">" + 
+      "Edit: <input type=\"text\" name=\"name\"/> " + 
+            "<input type=\"number\" name=\"price\" /> <br/>" +
+      "<input type=\"submit\" value=\"Submit\">" +
+    "</form>"
+
+  console.log(text);
+
+  $("#"+data[2]+"-editresult").html(text);
+  console.log("#"+data[2]+"-editresult");
+
+};
+
+
+
+//how to find which form????
+var $editform = $("#Alldaforms");
+//console.log($editform);
+
+$editform.on("submit", ".editForm", function(event) {
+  console.log('editing');
+
+  var $thisform = $(event.target).closest('form');
+
+  var ingrname = $thisform.find("[name=\"name\"]").val();
+  var ingrprice = $thisform.find("[name=\"price\"]").val();
+  var ingrid = $thisform.attr('id');
+
+  console.log([ingrname, ingrprice, ingrid]);
+
+  event.preventDefault();
+  $.get("editIngredient", {  //!!!!!
+    name: ingrname,
+    price: ingrprice,
+    id: ingrid,
+  }) 
+  .done(onEditSuccess)
+  .error(onError);
+
 });
--- assignments/burgers/routes/index.js
+++ assignments/burgers/routes/index.js
@@ -22,39 +22,42 @@ router.get('/ingredients', function(req, res, next){
 router.get('/addIngredient', function(req, res, next) { //!!!
     var newname = req.query.name;
     var newprice = req.query.price;
-    console.log([newname, newprice]);
+    //console.log([newname, newprice]);
 
     var newingr = new Ingredient({name: newname, price: newprice});
+    var id = newingr._id;
     //console.log(newingr.inStock);
 
     newingr.save(function(err) {
       if(err) console.log('Could not save');
-
     })
 
-    // Ingredient.find({}, function(err, ingredients) {
-    // var msg = "Ingredient names are: ";
-
-    // ingredients.forEach(function(ingredient){
-    //   msg = msg + ingredient.name + ",";
-    // })
-
-    // console.log(msg);
-    //res.render("ingredients", {allingredients: ingredients});
-    console.log([newname, newprice]);
-    res.send([newname, newprice]);
-
-
-    //res.send([newingr.name, newingr.price]);
-    //})
+    //console.log([newname, newprice]);
+    res.send([newname, newprice, id]);
 
 });
 
 router.get('/editIngredient', function(req, res, next) {
+  //if null, substitute original name/price?
   var newname = req.query.name;
   var newprice = req.query.price;
+  var id = req.query.id;
+  console.log(newname, newprice, id);
+  id = id.substring(0, id.length-10); //removes "-ajax-form" from id
+
+  console.log(newname, newprice, id);
+
+  Ingredient.findById(id, function (err, ingr) {
+    if(err) console.log('Could not edit');
+    console.log(ingr);
+    ingr.update({ name: newname, price: newprice }).exec();
+  });
+
   console.log(req.query);
   console.log([newname, newprice]);
+
+  res.send([newname, newprice, id]);
+
 
 
 });
