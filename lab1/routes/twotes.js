e6fb393d1f6907318b75b7678246c7213938a33a
dbishop <dbishop.rwc@gmail.com>
2016-02-26 15:32:09 -0500

    boilerplate

---
+++ lab1/routes/twotes.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+var Twote = require('../public/models/models.js').twoteModel;
+
+// Send list of all twotes
+router.get('/', function(req, res) {
+	getTwotesQuery().exec(function(err, twotes) {
+		res.json(twotes);
+	});
+});
+
+// Add new twote to database
+router.post('/new', function(req, res) {
+	var new_twote = new Twote(req.body);
+	new_twote.save(function(err) {
+		if (err) {
+			console.log("Problem adding new twote", err);
+		}
+	});
+});
+
+//Form a query that gets all twotes in the database
+function getTwotesQuery() {
+	return Twote.find({}, function(err) {
+		if (err) {
+			console.log("Problem fetching twotes", err);
+		}
+	});
+}
+
+module.exports = router;
+
