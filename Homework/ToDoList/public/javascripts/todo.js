17b9d08f5b4ccf5cb06b510c5610acb043eb05e9
rsiegel812 <robert.siegel@students.olin.edu>
2017-05-01 02:36:34 -0400

    All functionality except editing

--- Homework/ToDoList/public/javascripts/todo.js
+++ Homework/ToDoList/public/javascripts/todo.js
@@ -1,33 +1,13 @@
-
 var ToDoList = React.createClass({
   render: function() {
-    var tasks = this.props.tasks;
-    // if (this.props.sortBy == 'all'){
-    //   var taskList = tasks.map(function(individualTask){
-    //   return(
-    //     <ActiveTask task={individualTask.text} sortBy={this.props.sortBy} srchBy={individualTask} active={individualTask.active} handleDeleteTask={this.props.handleDeleteTask} handleEditTask={this.props.handleEditTask} handleCompletedTask={this.props.handleCompletedTask} />
-    //   )}, this);
-    // }
-    // else if (this.props.sortBy == 'active'){
-    //   var taskList = tasks.map(function(individualTask){
-    //   return(
-    //     <ActiveTask task={individualTask.text} sortBy={this.props.sortBy} srchBy={individualTask} active={individualTask.active} handleDeleteTask={this.props.handleDeleteTask} handleEditTask={this.props.handleEditTask} handleCompletedTask={this.props.handleCompletedTask} />
-    //   )}, this);
-    // }
-    // else if (this.props.sortBy == 'completed'){
-    //   var taskList = tasks.map(function(individualTask){
-    //   return(
-    //     <CompletedTask task={individualTask.text} sortBy={this.props.sortBy} srchBy={individualTask} active={individualTask.active} handleDeleteTask={this.props.handleDeleteTask} handleEditTask={this.props.handleEditTask} handleCompletedTask={this.props.handleCompletedTask} />
-    //   )}, this);
-    // } 
-    var taskList = tasks.map(function(individualTask){
-      return(
-        <ToDoListTask task={individualTask.text} sortBy={this.props.sortBy} srchBy={individualTask} active={individualTask.active} handleDeleteTask={this.props.handleDeleteTask} handleEditTask={this.props.handleEditTask} handleCompletedTask={this.props.handleCompletedTask} />
-      )}, this);
-
-    return (  
+  var tasks = this.props.tasks;
+  var taskList = tasks.map(function(individualTask){
+    return(
+      <ToDoListTask task={individualTask.text} sortBy={this.props.sortBy} srchBy={individualTask} active={individualTask.active} handleDeleteTask={this.props.handleDeleteTask} handleEditTask={this.props.handleEditTask} handleCompletedTask={this.props.handleCompletedTask} />
+    )}, this);
+    return (
       <div>
-         {taskList}
+        {taskList}
       </div>
     )
   }
@@ -43,62 +23,29 @@ var EditButton = React.createClass({
       <button type='button' onClick={this.onEditTask}> Edit </button>
     );
   }
-})
+});
 
 var DeleteButton = React.createClass({
   onDeleteTask: function(){
     this.props.handleDeleteTask(this.props.srchBy);
   },
-
+  
   render: function() {
     return (
       <button type='button' onClick={this.onDeleteTask}> Delete </button>
     );
   }
-})
+});
 
 var CompletedButton = React.createClass({
   onCompleteTask: function(){
     this.props.handleCompletedTask(this.props.srchBy);
   },
-
+  
   render: function() {
     return (
       <button type='button' onClick={this.onCompleteTask}> Finish </button>
     );
-  }
-})
-
-var CompletedTask = React.createClass({
-  render: function(){
-    if (this.props.active==false) {
-      return(
-        <li>
-        <del>{this.props.task}</del>
-        <DeleteButton {...this.props} />
-        <EditButton {...this.props} />
-        <CompletedButton {...this.props}  />
-      </li>
-      );
-    }
-    else {
-      return;
-    }
-  }
-})
-
-var ActiveTask = React.createClass({
-  render: function(){
-    if (this.props.active==true) {
-      return(
-        <li>
-        {this.props.task}
-        <DeleteButton {...this.props} />
-        <EditButton {...this.props} />
-        <CompletedButton {...this.props}  />
-      </li>
-      );
-    };
   }
 })
 
@@ -107,21 +54,21 @@ var ToDoListTask = React.createClass({
     if (this.props.active==true){
       return(
         <li>
-        {this.props.task}
-        <DeleteButton {...this.props} />
-        <EditButton {...this.props} />
-        <CompletedButton {...this.props}  />
-      </li>
+          {this.props.task}
+          <DeleteButton {...this.props} />
+          <EditButton {...this.props} />
+          <CompletedButton {...this.props} />
+        </li>
       );
-    }     
+    }
     else if (this.props.active==false){
       return(
-      <li>
-        <del>{this.props.task}</del>
-        <DeleteButton {...this.props} />
-        <EditButton {...this.props} />
-        <CompletedButton {...this.props}  />
-      </li>
+         <li>
+         <del>{this.props.task}</del>
+         <DeleteButton {...this.props} />
+         <EditButton {...this.props} />
+         <CompletedButton {...this.props} />
+        </li>
       );
     } 
     else {
@@ -129,8 +76,6 @@ var ToDoListTask = React.createClass({
     }
   }
 });
-
-
 
 var ToDoForm = React.createClass({
   getInitialState: function() {
@@ -148,21 +93,21 @@ var ToDoForm = React.createClass({
     this.setState({task: ''});
     React.findDOMNode(this.refs.task).focus();
     return;
-  },
+},
 
   onChange: function(e){
     this.setState({task: e.target.value});
   },
-  
+ 
   render: function(){
     return (
       <form onSubmit={this.handleSubmit}>
-        <input type='text'  placeholder='Add a Task' ref='task' onChange={this.onChange} value={this.state.task}/>
+        <input type='text' placeholder='Add a Task' ref='task' onChange={this.onChange} value={this.state.task}/>
         <input type='submit' value='Add'/>
       </form>
     );
   }
-});   
+});
 
 var ToDoCount = React.createClass({
   render: function() {
@@ -177,11 +122,10 @@ var SortTasks = React.createClass({
   updateFilter: function(e){
     this.props.handleUpdateFilter(e.target.value);
   },
-
   render: function() {
     return (
       <form onChange={this.updateFilter}>
-        <input type='radio' name='sortBy' value='all' defaultChecked={true}/> All 
+        <input type='radio' name='sortBy' value='all' defaultChecked={true}/> All
         <input type='radio' name='sortBy' value='active'/> Active
         <input type='radio' name='sortBy' value='completed'/> Completed
       </form>
@@ -197,7 +141,7 @@ var ToDoApp = React.createClass({
     };
   },
 
-  //handles a new task being added to the state
+//handles a new task being added to the state
   updateTasks: function(newTask) {
     var updatedTasks = this.state.tasks.concat({text: newTask, active:true, key: Math.random().toString(36).substr(2, 9)});
     this.setState({tasks: updatedTasks});
@@ -209,7 +153,6 @@ var ToDoApp = React.createClass({
 
   handleEditTask: function(taskObj) {
     var index = this.state.tasks.indexOf(taskObj);
-    this.state.tasks[index].active = false;
   },
 
   handleDeleteTask: function(taskObj){
@@ -227,34 +170,40 @@ var ToDoApp = React.createClass({
     }
     this.setState({tasks: tasks})
   },
-
+  
   render: function() {
     var tasks = [];
+    var activeTasks = []
+    for (var i in this.state.tasks) {
+      if (this.state.tasks[i].active == true) {
+        activeTasks = activeTasks.concat(this.state.tasks[i]);
+      }
+    }
+
     if (this.state.sortBy == 'all') {
       tasks = this.state.tasks;
     }
     else if (this.state.sortBy == 'active') {
-      tasks = this.state.tasks.filter(function(task){
-        return task.active = true;
-      });
+      tasks = activeTasks;
     }
     else if (this.state.sortBy == 'completed') {
-      tasks = this.state.tasks.filter(function(task){
-        return task.active = false;
-      });
+      for (var i in this.state.tasks) {
+        if (this.state.tasks[i].active == false) {
+          tasks = tasks.concat(this.state.tasks[i]);
+        }   
+      }
     }
     return (
       <div>
         <h1>To Do List</h1>
         <ToDoList tasks={tasks} sortBy={this.state.sortBy} handleDeleteTask={this.handleDeleteTask} handleEditTask={this.handleEditTask} handleCompletedTask={this.handleCompletedTask}/>
         <ToDoForm onFormSubmit={this.updateTasks}/>
-        <ToDoCount tasks={this.state.tasks}/>
+        <ToDoCount tasks={activeTasks}/>
         <SortTasks handleUpdateFilter={this.handleUpdateFilter}/>
       </div>
     );
   }
 });
-
 
 ReactDOM.render(
   <ToDoApp/>,
